109fed8ae0b3541f5ac34c158334ff6f22233237
Gabriel <gabriel.zanatto2@gmail.com>
2019-05-23 11:57:19 -0300

    criando requisito de dados

--- nanoChallenge2/Controller/ViewController.swift
+++ nanoChallenge2/Controller/ViewController.swift
@@ -9,13 +9,78 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var personagens = [Personagem]()
     @IBOutlet weak var teble: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        teble.dataSource = self
+        teble.delegate = self
+        
+        guard let url = URL(string: "https://breakingbadapi.com/api/characters") else { return }
+        
+        let session = URLSession.shared
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let task = session.dataTask(with: url, completionHandler: { data, response, error in
+            guard let response = response else {
+                return
+            }
+            guard error == nil else {
+                return
+            }
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] {
+                    print(json)
+                }
+            } catch let erro {
+                print(erro.localizedDescription)
+            }
+        })
+        task.resume()
+        
+        
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate{
+    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        performSegue(withIdentifier: "detail", sender: )
+//    }
+//
+//
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if let nextScreenVC = segue.destination as? DetalheViewController, let data = sender as? Palestras {
+//            nextScreenVC.palestra = data
+//        }
+//    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return personagens.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CellTableViewCell else { fatalError() }
+        
+        cell.nome.text = personagens[indexPath.row].nome
+        cell.id = personagens[indexPath.row].id
+        cell.img.image = UIImage(named: personagens[indexPath.row].img)
+        
+        return cell
+    }
+    
+    
+}

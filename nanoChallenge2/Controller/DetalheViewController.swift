f57cd14ddd1663fbab4b3448dd765e3904818a40
Gabriel <gabriel.zanatto2@gmail.com>
2019-05-23 11:18:16 -0300

    add nova teal no story board

---
+++ nanoChallenge2/Controller/DetalheViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetalheViewController.swift
+//  nanoChallenge2
+//
+//  Created by Gabriel Zanatto Salami on 23/05/19.
+//  Copyright © 2019 Gabriel Zanatto Salami. All rights reserved.
+//
+
+import UIKit
+
+class DetalheViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
